c6313954734f0d11dd79f25a622e748f8a23e9af
Shahzad-Akbar <shad80909@gmail.com>
2025-01-01 21:10:25 +0530

    auth user and controller is setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {signup, login, logout, getMe } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
-
+//now protectRoute is user for providing security
 router.post("/me", protectRoute, getMe);
 
 router.post("/signup", signup);

7d498647e58f3b6a55f3cdb2d78ac796cd85d566
Shahzad-Akbar <shad80909@gmail.com>
2025-01-26 21:42:34 +0530

    3- create post with uploading image handeled

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const PORT= process.env.PORT || 5000;
 
-app.use(express.json()); //to parse req.body
+app.use(express.json({limit: "5mb"})); //to parse req.body and limit the size of image to 5mb
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
